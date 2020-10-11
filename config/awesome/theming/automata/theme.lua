a6abf1e92dd48fe12d53d2b7339430ff547297ee
Leo Gaskin <leo.gaskin@brg-feldkirchen.at>
2020-10-11 20:53:18 +0200

    Simplify AwesomeWM theme

--- config/awesome/theming/automata/theme.lua
+++ config/awesome/theming/automata/theme.lua
@@ -70,24 +70,24 @@ theme.border_normal = theme.white
 theme.border_focus  = theme.magenta
 theme.border_marked = theme.blue
 
--- There are other variable sets
--- overriding the default one when
+-- There are other variable sets overriding the default one when
 -- defined, the sets are:
--- taglist_[bg|fg]_[focus|urgent|occupied|empty|volatile]
--- tasklist_[bg|fg]_[focus|urgent]
--- titlebar_[bg|fg]_[normal|focus]
--- tooltip_[font|opacity|fg_color|bg_color|border_width|border_color]
--- mouse_finder_[color|timeout|animate_timeout|radius|factor]
--- prompt_[fg|bg|fg_cursor|bg_cursor|font]
--- hotkeys_[bg|fg|border_width|border_color|shape|opacity|modifiers_fg|label_bg|label_fg|group_margin|font|description_font]
+
+-- theme.taglist_[bg|fg]_[focus|urgent|occupied|empty|volatile]
+-- theme.tasklist_[bg|fg]_[focus|urgent]
+-- theme.titlebar_[bg|fg]_[normal|focus]
+-- theme.tooltip_[font|opacity|fg_color|bg_color|border_width|border_color]
+-- theme.mouse_finder_[color|timeout|animate_timeout|radius|factor]
+-- theme.prompt_[fg|bg|fg_cursor|bg_cursor|font]
+-- theme.hotkeys_[bg|fg|border_width|border_color|shape|opacity|modifiers_fg|label_bg|label_fg|group_margin|font|description_font]
 
 -- Variables set for theming notifications:
--- notification_font
+-- theme.notification_font
 theme.notification_bg = "#151718"
 theme.notification_fg = "#D6D6D6"
 theme.notification_border_color = "#151718"
--- notification_[width|height|margin]
--- notification_[border_color|border_width|shape|opacity]
+-- theme.notification_[width|height|margin]
+-- theme.notification_[border_color|border_width|shape|opacity]
 theme.notification_icon_size = 200
 theme.notification_max_height = 200
 theme.notification_max_width = 400
@@ -95,62 +95,16 @@ theme.notification_min_height = 100
 theme.notification_min_width = 400
 
 -- Variables set for theming the menu:
--- menu_[bg|fg]_[normal|focus]
--- menu_[border_color|border_width]
+-- theme.menu_[bg|fg]_[normal|focus]
+-- theme.menu_[border_color|border_width]
 theme.menu_submenu_icon = theme_path.."default/submenu.png"
 theme.menu_height = dpi(15)
 theme.menu_width  = dpi(100)
-
--- Define the image to load
-theme.titlebar_close_button_normal = theme_path.."default/titlebar/close_normal.png"
-theme.titlebar_close_button_focus  = theme_path.."default/titlebar/close_focus.png"
-
-theme.titlebar_minimize_button_normal = theme_path.."default/titlebar/minimize_normal.png"
-theme.titlebar_minimize_button_focus  = theme_path.."default/titlebar/minimize_focus.png"
-
-theme.titlebar_ontop_button_normal_inactive = theme_path.."default/titlebar/ontop_normal_inactive.png"
-theme.titlebar_ontop_button_focus_inactive  = theme_path.."default/titlebar/ontop_focus_inactive.png"
-theme.titlebar_ontop_button_normal_active = theme_path.."default/titlebar/ontop_normal_active.png"
-theme.titlebar_ontop_button_focus_active  = theme_path.."default/titlebar/ontop_focus_active.png"
-
-theme.titlebar_sticky_button_normal_inactive = theme_path.."default/titlebar/sticky_normal_inactive.png"
-theme.titlebar_sticky_button_focus_inactive  = theme_path.."default/titlebar/sticky_focus_inactive.png"
-theme.titlebar_sticky_button_normal_active = theme_path.."default/titlebar/sticky_normal_active.png"
-theme.titlebar_sticky_button_focus_active  = theme_path.."default/titlebar/sticky_focus_active.png"
-
-theme.titlebar_floating_button_normal_inactive = theme_path.."default/titlebar/floating_normal_inactive.png"
-theme.titlebar_floating_button_focus_inactive  = theme_path.."default/titlebar/floating_focus_inactive.png"
-theme.titlebar_floating_button_normal_active = theme_path.."default/titlebar/floating_normal_active.png"
-theme.titlebar_floating_button_focus_active  = theme_path.."default/titlebar/floating_focus_active.png"
-
-theme.titlebar_maximized_button_normal_inactive = theme_path.."default/titlebar/maximized_normal_inactive.png"
-theme.titlebar_maximized_button_focus_inactive  = theme_path.."default/titlebar/maximized_focus_inactive.png"
-theme.titlebar_maximized_button_normal_active = theme_path.."default/titlebar/maximized_normal_active.png"
-theme.titlebar_maximized_button_focus_active  = theme_path.."default/titlebar/maximized_focus_active.png"
-
--- You can use your own layout icons like this:
-theme.layout_fairh = theme_path.."default/layouts/fairhw.png"
-theme.layout_fairv = theme_path.."default/layouts/fairvw.png"
-theme.layout_floating  = theme_path.."default/layouts/floatingw.png"
-theme.layout_magnifier = theme_path.."default/layouts/magnifierw.png"
-theme.layout_max = theme_path.."default/layouts/maxw.png"
-theme.layout_fullscreen = theme_path.."default/layouts/fullscreenw.png"
-theme.layout_tilebottom = theme_path.."default/layouts/tilebottomw.png"
-theme.layout_tileleft   = theme_path.."default/layouts/tileleftw.png"
-theme.layout_tile = theme_path.."default/layouts/tilew.png"
-theme.layout_tiletop = theme_path.."default/layouts/tiletopw.png"
-theme.layout_spiral  = theme_path.."default/layouts/spiralw.png"
-theme.layout_dwindle = theme_path.."default/layouts/dwindlew.png"
-theme.layout_cornernw = theme_path.."default/layouts/cornernww.png"
-theme.layout_cornerne = theme_path.."default/layouts/cornernew.png"
-theme.layout_cornersw = theme_path.."default/layouts/cornersww.png"
-theme.layout_cornerse = theme_path.."default/layouts/cornersew.png"
 
 -- Generate Awesome icon:
 theme.awesome_icon = theme_assets.awesome_icon(
     theme.menu_height, theme.bg_focus, theme.fg_focus
 )
-
 -- Define the icon theme for application icons. If not set then the icons
 -- from /usr/share/icons and /usr/share/icons/hicolor will be used.
 theme.icon_theme = nil
