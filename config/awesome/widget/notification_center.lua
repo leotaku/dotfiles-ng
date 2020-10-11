fcc323ee969ee7536c463ea7b2b04a4c5d3d8c39
Leo Gaskin <leo.gaskin@brg-feldkirchen.at>
2020-10-11 20:58:10 +0200

    Fix notification center configuration

--- config/awesome/widget/notification_center.lua
+++ config/awesome/widget/notification_center.lua
@@ -1,65 +1,104 @@
-local sidebar = wibox {
+local wibox = require("wibox")
+local log = require("lib.log")
+local dpi = require("beautiful").xresources.apply_dpi
+local inspect = require("lib.inspect")
+
+local notif_wb = wibox {
    visible = true,
    ontop = false,
-   widget = wibox.layout.align.vertical,
-   ---
    screen = screen.primary,
-   height = screen.primary.geometry.height - 33,
-   width = 400,
+   height = screen.primary.geometry.height - dpi(20),
+   width = dpi(200),
+   opacity = 1,
+}
+awful.placement.bottom_right(notif_wb)
+
+local notif_list = wibox.layout.fixed.vertical()
+
+notif_list:setup {
+   nil,
+   layout = wibox.layout.fixed.vertical,
 }
 
-local timeout = gears.timer {
-   timeout   = 6,
+notif_wb:setup {
+   nil,
+   notif_list,
+   layout = wibox.layout.align.vertical,
+}
+
+local notif_timeout = gears.timer {
+   timeout   = 3,
    call_now  = false,
    autostart = false,
    single_shot = true;
    callback  = function()
-      sidebar.ontop = false
+      notif_wb.ontop = false
    end
 }
 
-local placement = awful.placement.bottom_right
-placement(sidebar)
+-- Handle notification icon
+naughty.connect_signal(
+   "request::icon",
+   function(n, context, hints)
+      -- Handle other contexts here
+      if context ~= "app_icon" then return end
 
-local function get_color(urgency)
+      -- Use XDG icon
+      local path = menubar.utils.lookup_icon(hints.app_icon) or
+         menubar.utils.lookup_icon(hints.app_icon:lower())
+
+      if path then
+         n.icon = path
+      end
+   end
+)
+
+-- Use XDG icon
+naughty.connect_signal(
+   "request::action_icon",
+   function(a, context, hints)
+      a.icon = menubar.utils.lookup_icon(hints.id)
+   end
+)
+
+local function urgency_color(urgency)
    if urgency == "critical"  then
       return beautiful.bg_urgent
    else
-      return beautiful.green
+      return beautiful.blue
    end
 end
 
-local function create_notification_item(n)
-   return {
+local function notif_box(title, message, urgency)
+   return wibox.widget {
       {
          {
-            wibox.widget.textbox("<b>"..n.title.."</b>"),
-            wibox.widget.textbox(n.text),
-            layout = wibox.layout.fixed.vertical
+            {
+               markup = "<b>" .. title .. "</b>",
+               widget = wibox.widget.textbox,
+            },
+            {
+               markup = message,
+               widget = wibox.widget.textbox,
+            },
+            layout = wibox.layout.align.vertical,
          },
          margins = dpi(10),
-         layout = wibox.container.margin
+         layout = wibox.container.margin,
       },
-      color = get_color(n.urgency),
+      color = urgency_color(urgency),
       left = dpi(5),
-      layout = wibox.container.margin
+      layout = wibox.container.margin,
    }
 end
 
 naughty.connect_signal(
    "request::display",
    function(n)
-      local children = sidebar.widget:get_children()
-      local item = create_notification_item(n)
-
-      local l = wibox.layout.fixed.vertical()
-      l:add(item)
-      for i, child in ipairs(children) do
-         l:add(child)
-      end
-
-      sidebar.widget = l
-      sidebar.ontop = true
-      timeout:start()
+      local box = notif_box(n.title, n.message, n.urgency)
+      notif_list:insert(1, box)
+      notif_wb.ontop = true
+      notif_timeout:stop()
+      notif_timeout:start()
    end
 )
