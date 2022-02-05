8cb51dd8d7de6a0a3ea234a1afaf5e1788fcfdef
Leo Gaskin <leo.gaskin@le0.gs>
2022-02-05 08:13:00 +0100

    Increase redshift color range

--- systemd/redshift.sh
+++ systemd/redshift.sh
@@ -3,4 +3,4 @@ set -e
 URL="https://location.services.mozilla.com/v1/geolocate?key=geoclue"
 LATLONG="$(curl "$URL" | jq -r '.location | "\(.lat):\(.lng)"')"
 
-redshift -l "$LATLONG" -t 4800:3000 -b 1 1
+redshift -l "$LATLONG" -t 4800:2000 -b 1 1
