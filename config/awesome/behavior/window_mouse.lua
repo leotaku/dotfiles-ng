3a96cfd41553cf17020bd1a0860883ef3de3065c
Leo Gaskin <leo.gaskin@le0.gs>
2022-02-22 15:37:47 +0100

    Streamline window_mouse implementation

--- config/awesome/behavior/window_mouse.lua
+++ config/awesome/behavior/window_mouse.lua
@@ -4,14 +4,14 @@
 local gears = require("gears")
 
 -- Code
-local function move_mouse(c)
+local function move_mouse(this)
    gears.timer.delayed_call(
       function ()
-         if c.valid then
+         if this.valid then
             mouse.coords (
                {
-                  x = c.x + c.width  * (c.rel_x or 0.5),
-                  y = c.y + c.height * (c.rel_y or 0.5),
+                  x = this.x + this.width  * (this.rel_x or 0.5),
+                  y = this.y + this.height * (this.rel_y or 0.5),
                },
                true
             )
@@ -20,19 +20,28 @@ local function move_mouse(c)
    )
 end
 
-local function set_mouse(c)
-   if c then
-      local m = mouse.coords()
-      c.rel_x = (m.x - c.x) / c.width
-      c.rel_y = (m.y - c.y) / c.height
+local function set_mouse(this)
+   if this then
+      local mouse = mouse.coords()
+      this.rel_x = (mouse.x - this.x) / this.width
+      this.rel_y = (mouse.y - this.y) / this.height
    end
 end
 
 local inhibit_change_signals = true
 
+local function handle_change_signal(this)
+   if inhibit_change_signals then
+      return
+   end
+
+   set_mouse(mouse.current_client)
+   move_mouse(this)
+end
+
 client.connect_signal(
-   "manage",
-   function (c)
+   "request::manage",
+   function (this)
       inhibit_change_signals = true
       gears.timer.delayed_call(
          function ()
@@ -45,34 +54,15 @@ client.connect_signal(
 
 client.connect_signal(
    "focus",
-   function (c)
-      if not (c == mouse.current_client) then
+   function (this)
+      if not (this == mouse.current_client) then
          set_mouse(mouse.current_client)
-         move_mouse(c)
+         move_mouse(this)
       end
    end
 )
 
-client.connect_signal(
-   "swapped",
-   function(c, C)
-      if inhibit_change_signals then
-         return
-      end
-
-      set_mouse(mouse.current_client)
-      move_mouse(c)
-   end
-)
-
-client.connect_signal(
-   "property::floating",
-   function(c)
-      if inhibit_change_signals then
-         return
-      end
-
-      set_mouse(mouse.current_client)
-      move_mouse(c)
-   end
-)
+client.connect_signal("swapped", handle_change_signal)
+client.connect_signal("property::floating", handle_change_signal)
+client.connect_signal("property::fullscreen", handle_change_signal)
+client.connect_signal("property::maximized", handle_change_signal)
