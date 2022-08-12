4eab977ffb6d19221da49c2d11299340eb945743
Leo Gaskin <leo.gaskin@le0.gs>
2022-08-12 05:59:05 +0200

    Use new AwesomeWM API for rules

--- config/awesome/behavior/rules.lua
+++ config/awesome/behavior/rules.lua
@@ -1,34 +1,28 @@
 -- Rules for awesomewm clients
 
 -- Imports
-local awful = require("awful")
-local gears = require("gears")
+local ruled = require("ruled")
 
 -- Code
-awful.rules.rules = gears.table.join(
-   awful.rules.rules,
+ruled.client.append_rules {
+   -- Floating clients
    {
-      -- Floating clients
-      {
-         rule_any = {
-            name = {
-               "Event Tester",
-               "Color Picker",
-            },
+      rule_any = {
+         name = {
+            "Event Tester",
+            "Color Picker",
          },
-
-         properties = { floating = true },
       },
-
-      -- Ignore size hints for most windows
-      {
-         rule_any = {
-            class = {
-               "kitty", "Kitty",
-               "emacs", "Emacs",
-            },
+      properties = { floating = true },
+   },
+   -- Ignore size hints for most windows
+   {
+      rule_any = {
+        class = {
+            "kitty", "Kitty",
+            "emacs", "Emacs",
          },
-         properties = { size_hints_honor = false }
       },
-   }
-)
+      properties = { size_hints_honor = false },
+   },
+}
