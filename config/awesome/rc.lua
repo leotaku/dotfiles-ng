90830e19b804d1ae5195a32c0f576f353e890f74
Leo Gaskin <leo.gaskin@le0.gs>
2022-08-19 12:54:12 +0200

    Fix AwesomeWM mouse client moveing and resizing

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -123,6 +123,20 @@ awful.screen.connect_for_each_screen(
    end
 )
 
+local clientbuttons = gears.table.join(
+    awful.button({ }, 1, function (c)
+        c:emit_signal("request::activate", "mouse_click", {raise = true})
+    end),
+    awful.button({ modkey }, 1, function (c)
+        c:emit_signal("request::activate", "mouse_click", {raise = true})
+        awful.mouse.client.move(c)
+    end),
+    awful.button({ modkey }, 3, function (c)
+        c:emit_signal("request::activate", "mouse_click", {raise = true})
+        awful.mouse.client.resize(c)
+    end)
+)
+
 awful.rules.rules = gears.table.join(
    awful.rules.rules,
    {
@@ -132,6 +146,7 @@ awful.rules.rules = gears.table.join(
             border_width = beautiful.border_width,
             border_color = beautiful.border_normal,
             focus = awful.client.focus.filter,
+            buttons = clientbuttons,
             raise = true,
             screen = awful.screen.preferred,
             placement = awful.placement.no_overlap+awful.placement.no_offscreen,
