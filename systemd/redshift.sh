8fe63749abd51186eac8a99013f6d099f0430530
Leo Gaskin <leo.gaskin@le0.gs>
2022-09-22 17:53:11 +0200

    Add default location for redshift

--- systemd/redshift.sh
+++ systemd/redshift.sh
@@ -2,5 +2,8 @@
 set -e
 URL="https://location.services.mozilla.com/v1/geolocate?key=geoclue"
 LATLONG="$(curl "$URL" | jq -r '.location | "\(.lat):\(.lng)"')"
+if [ -z "$LATLONG" ]; then
+    LATLONG="48.217:16.3944"
+fi
 
 redshift -l "$LATLONG" -t 4800:2000 -b 1 1
