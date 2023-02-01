30fb9aeec2911d43c31bb04c9cdd8607fbf7f28a
Leo Gaskin <leo.gaskin@le0.gs>
2023-02-01 16:58:26 +0100

    Move resizing windows from right click to left+shift

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -131,7 +131,7 @@ local clientbuttons = gears.table.join(
         c:emit_signal("request::activate", "mouse_click", {raise = true})
         awful.mouse.client.move(c)
     end),
-    awful.button({ modkey }, 3, function (c)
+    awful.button({ modkey, "Shift" }, 1, function (c)
         c:emit_signal("request::activate", "mouse_click", {raise = true})
         awful.mouse.client.resize(c)
     end)
