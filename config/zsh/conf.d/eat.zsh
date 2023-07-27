22fff835ca4318055fa9237ef2b038639d4aa904
Leo Gaskin <leo.gaskin@le0.gs>
2023-07-27 13:52:30 +0200

    Limit emacs-eat integration to just directory tracking

--- config/zsh/conf.d/eat.zsh
+++ config/zsh/conf.d/eat.zsh
@@ -1,3 +1,11 @@
 if [[ -n "$EAT_SHELL_INTEGRATION_DIR" ]]; then
-    source "$EAT_SHELL_INTEGRATION_DIR/zsh"
+    autoload -Uz add-zsh-hook
+    BASE64_HOST="$(base64 <<< "$HOST")"
+
+    __eat_chpwd () {
+        printf '\e]51;e;A;%s;%s\e\\' "$BASE64_HOST" \
+               "$(base64 <<< "$PWD")"
+    }
+
+    add-zsh-hook chpwd __eat_chpwd
 fi
