db5f4c6648c609d7c581e23e1119e79b9cde7da0
Leo Gaskin <leo.gaskin@le0.gs>
2023-10-04 11:58:22 +0200

    Remove no longer used service definitions

--- systemd/redshift.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-set -e
-URL="https://location.services.mozilla.com/v1/geolocate?key=geoclue"
-LATLONG="$(curl "$URL" | jq -r '.location | "\(.lat):\(.lng)"')"
-if [ -z "$LATLONG" ]; then
-    LATLONG="48.217:16.3944"
-fi
-
-redshift -l "$LATLONG" -t 4800:2000 -b 1 1
