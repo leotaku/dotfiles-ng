754e16833ce135364e70a12b212b92f07069eb62
Leo Gaskin <leo.gaskin@le0.gs>
2023-10-26 20:42:12 +0200

    Fix __eat_chpwd ZSH hook implementation

--- config/zsh/conf.d/eat.zsh
+++ config/zsh/conf.d/eat.zsh
@@ -1,10 +1,10 @@
 if [[ -n "$EAT_SHELL_INTEGRATION_DIR" ]]; then
     autoload -Uz add-zsh-hook
-    BASE64_HOST="$(base64 <<< "$HOST")"
+    __EAT_BASE64_HOST="$(printf "%s" "$HOST" | base64)"
 
     __eat_chpwd () {
-        printf '\e]51;e;A;%s;%s\e\\' "$BASE64_HOST" \
-               "$(base64 <<< "$PWD")"
+        printf '\e]51;e;A;%s;%s\e\\' "$__EAT_BASE64_HOST" \
+               "$(printf "%s" "$PWD" | base64)"
     }
 
     add-zsh-hook chpwd __eat_chpwd
